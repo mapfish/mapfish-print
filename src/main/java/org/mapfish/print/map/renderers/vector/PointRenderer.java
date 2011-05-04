16b0a9ab085475e9cf28e700b3249d1ddbed07c7
sbrunner <stephane.brunner@camptocamp.com>
2011-05-04 10:40:38 +0000

    Point visible on label ... (closes #623)
    git-svn-id: http://www.mapfish.org/svn/mapfish/print/trunk@3774 25cca10d-0837-0410-9e4f-92b931fda1bc

--- src/main/java/org/mapfish/print/map/renderers/vector/PointRenderer.java
+++ src/main/java/org/mapfish/print/map/renderers/vector/PointRenderer.java
@@ -90,7 +90,10 @@ public class PointRenderer extends GeometriesRenderer<Point> {
         float height = style.optFloat("graphicHeight", pointRadius * 2.0f);
         float offsetX = style.optFloat("graphicXOffset", -width / 2.0f);
         float offsetY = style.optFloat("graphicYOffset", -height / 2.0f);
-
+        
+        // See Feature/Vector.js for more information about labels 
+        String label = style.optString("label");
+        
         if (style.optString("externalGraphic") != null) {
             float opacity = style.optFloat("graphicOpacity", style.optFloat("fillOpacity", 1.0f));
             state.setFillOpacity(opacity);
@@ -121,8 +124,9 @@ public class PointRenderer extends GeometriesRenderer<Point> {
             }
             dc.closePath();
             dc.fillStroke();
-
-		} else {
+        } else if (label != null && label.length() > 0) {
+            // rendered by LabelRenderer
+        } else {
             PolygonRenderer.applyStyle(context, dc, style, state);
             dc.setGState(state);
 
