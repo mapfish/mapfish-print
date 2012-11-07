ad6f01161ed3574e9a2ce2c92824a7c398d668fb
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-11-07 09:14:10 +0100

    updated to gradle 1.2

--- project/Build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object PrintBuild extends Build {
-    lazy val root = Project(id = "mapfish-print",
-                            base = file("."))
-                            
-  val springVersion = SettingKey[String]("spring-version", "The version of Spring.")
-}
