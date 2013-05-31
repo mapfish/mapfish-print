8566e698340bbd1c8fa176377c8788cb8c249ea5
jbjonesjr <jbjonesjr@gmail.com>
2013-05-31 19:56:08 -0400

    Laying the ground work for upcoming TMS layer tests

--- src/test/java/org/mapfish/print/map/readers/TMSLayerTest.java
+++ src/test/java/org/mapfish/print/map/readers/TMSLayerTest.java
@@ -1,36 +1,154 @@
 package org.mapfish.print.map.readers;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.pdf.PdfWriter;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
+import org.mapfish.print.MapPrinter;
+import org.mapfish.print.PrintTestCase;
+import org.mapfish.print.RenderingContext;
 import org.mapfish.print.ShellMapPrinter;
+import org.mapfish.print.config.Config;
+import org.mapfish.print.config.HostMatcher;
+import org.mapfish.print.config.layout.Block;
+import org.mapfish.print.config.layout.Layout;
+import org.mapfish.print.config.layout.MainPage;
+import org.mapfish.print.config.layout.MapBlock;
+import org.mapfish.print.utils.PJsonObject;
+import org.pvalsecc.misc.FileUtilities;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.*;
 
 import static org.junit.Assert.assertNotNull;
 
-public class TMSLayerTest {
+public class TMSLayerTest extends PrintTestCase {
 
-    @Override
+    private static final int MARGIN = 40;
+
+
+    private TmsMapReader tmsreader;
+    protected Document doc;
+    protected RenderingContext context;
+    private PdfWriter writer;
+    private OutputStream outFile;
+
+    PJsonObject tmsSpec;
+
+
+    public TMSLayerTest(String name) {
+        super(name);
+    }
+
     protected void setUp() throws Exception {
+        super.setUp();
+        //  PJsonObject spec = MapPrinter.parseSpec(FileUtilities.readWholeTextFile(new File(XYZLayerTest.class.getClassLoader().getResource("samples/spec.json").getFile())));
+        // spec.getInternalObj().put("units", "meters");
+
+        doc = new Document(PageSize.A4);
+        String baseDir = getBaseDir();
+        outFile = new FileOutputStream(baseDir + getClass().getSimpleName() + "_" + getName() + ".pdf");
+        writer = PdfWriter.getInstance(doc, outFile);
+        writer.setFullCompression();
+        Layout layout = new Layout();
+        MainPage mainPage = new MainPage();
+        final MapBlock mapBlock = new MapBlock();
+        mainPage.setItems(new ArrayList<Block>(Arrays.asList(mapBlock)));
+        layout.setMainPage(mainPage);
+        Config config = new Config();
+        config.setDpis(new TreeSet<Integer>(Arrays.asList(96, 190, 254)));
+        config.setScales(new TreeSet<Integer>(Arrays.asList(20000, 25000, 100000, 500000, 4000000)));
+        List<HostMatcher> hosts = new ArrayList<HostMatcher>(1);
+        hosts.add(HostMatcher.ACCEPT_ALL);
+        config.setHosts(hosts);
+        context = new RenderingContext(doc, writer, config, null, null, layout, Collections.<String, String>emptyMap());
+
+
+        tmsSpec = MapPrinter.parseSpec(FileUtilities.readWholeTextFile(
+                new File(TMSLayerTest.class.getClassLoader().getResource("layers/layer_spec.json").getFile())
+        ));
 
     }
 
-    @Override
     protected void tearDown() throws Exception {
+        writer.close();
+        outFile.close();
 
-        //super.tearDown();
+        context = null;
+
+        super.tearDown();
     }
 
-        @Test
-	public void testNoOrigin() {
-		MapReaderFactoryFinder ff = new ClassPathXmlApplicationContext(ShellMapPrinter.DEFAULT_SPRING_CONTEXT).getBean(MapReaderFactoryFinder.class);
+	public void testNoOrigin() throws JSONException{
 
-            //"Preparing for future tests
-            assertTrue(true);
-	}
+        JSONObject tms_full = tmsSpec.getInternalObj();
+        tms_full.accumulate("tileOrigin", null);
+        tms_full.accumulate("origin", null);
+        tmsSpec = new PJsonObject(tms_full, "");
 
-    @Test
-    public void testOrigin() {
-        //"Preparing for future tests
-        assertTrue(true);
+        tmsreader = new TmsMapReader("foo", context, tmsSpec);
+
+
+        //assertEquals("Default format (null path_format) did not get created correctly", , );
+
+    }
+
+    public void testOriginXY() throws JSONException{
+        JSONObject tms_full = tmsSpec.getInternalObj();
+        tms_full.accumulate("tileOrigin", null);
+        tms_full.accumulate("origin", null);
+        tmsSpec = new PJsonObject(tms_full, "");
+
+        tmsreader = new TmsMapReader("foo", context, tmsSpec);
+
+
+        //assertEquals("Default format (null path_format) did not get created correctly", , );
+    }
+
+    public void testTileOriginXY() throws JSONException{
+        JSONObject tms_full = tmsSpec.getInternalObj();
+        tms_full.accumulate("tileOrigin", null);
+        tms_full.accumulate("origin", null);
+        tmsSpec = new PJsonObject(tms_full, "");
+
+        tmsreader = new TmsMapReader("foo", context, tmsSpec);
+
+
+        //assertEquals("Default format (null path_format) did not get created correctly", , );
+    }
+
+    public void testOriginLatLon() throws JSONException{
+        JSONObject tms_full = tmsSpec.getInternalObj();
+        tms_full.accumulate("tileOrigin", null);
+        tms_full.accumulate("origin", null);
+        tmsSpec = new PJsonObject(tms_full, "");
+
+        tmsreader = new TmsMapReader("foo", context, tmsSpec);
+
+
+        //assertEquals("Default format (null path_format) did not get created correctly", , );
+    }
+
+    private String getBaseDir() {
+        //This test expects to be able to write files into the same directory the classes
+        //are compiled to, in this case the build/classes/test directory
+        String expectedPath = "build" + File.separator + "classes" + File.separator + "test";
+        String baseDir = XYZLayerTest.class.getClassLoader().getResource(".").getFile();
+        if (baseDir.indexOf("pulse-java.jar") != -1) {
+            String[] paths = System.getProperty("java.class.path").split(File.pathSeparator);
+
+            for (String path : paths) {
+                if (path.indexOf(expectedPath) != -1 || path.indexOf("out/test/mapfish-print") != -1) {
+                    baseDir = path;
+                }
+            }
+        }
+        return baseDir;
     }
 
 }
