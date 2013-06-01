cc06bb04b926e1564f39114a513c73b50d3e22bf
jbjonesjr <jbjonesjr@gmail.com>
2013-05-31 23:38:34 -0400

    Unit tests that handle any of the origin param options AND no params.

--- src/test/java/org/mapfish/print/map/readers/TMSLayerTest.java
+++ src/test/java/org/mapfish/print/map/readers/TMSLayerTest.java
@@ -93,32 +93,31 @@ public class TMSLayerTest extends PrintTestCase {
 
         tmsreader = new TmsMapReader("foo", context, tmsSpec);
 
-
-        //assertEquals("Default format (null path_format) did not get created correctly", , );
-
+        assertEquals("Origin X is not initiated by default as expected",0.0f,tmsreader.tileCacheLayerInfo.originX);
+        assertEquals("Origin Y is not initiated by default as expected",0.0f,tmsreader.tileCacheLayerInfo.originY);
     }
 
     public void testOriginXY() throws JSONException{
         JSONObject tms_full = tmsSpec.getInternalObj();
         Map<String, Float> origin = new HashMap<String, Float>();
         origin.put("x",-10.0f);
-        origin.put("y",-10.0f);
+        origin.put("y",-20.0f);
 
         tms_full.accumulate("tileOrigin", null);
-        tms_full.accumulate("origin", origin);
+        tms_full.accumulate("origin", new JSONObject(origin));
         tmsSpec = new PJsonObject(tms_full, "");
 
         tmsreader = new TmsMapReader("foo", context, tmsSpec);
 
-
-        //assertEquals("Default format (null path_format) did not get created correctly", , );
+        assertEquals("Origin X is not set (via origin.x) as expected",-10.0f,tmsreader.tileCacheLayerInfo.originX);
+        assertEquals("Origin Y is not set (via origin.y) as expected",-20.0f,tmsreader.tileCacheLayerInfo.originY);
     }
 
     public void testTileOriginXY() throws JSONException{
         JSONObject tms_full = tmsSpec.getInternalObj();
         Map<String, Float> origin = new HashMap<String, Float>();
         origin.put("x",-10.0f);
-        origin.put("y",-10.0f);
+        origin.put("y",-20.0f);
 
         tms_full.accumulate("tileOrigin", new JSONObject(origin));
         tms_full.accumulate("origin", null);
@@ -126,14 +125,14 @@ public class TMSLayerTest extends PrintTestCase {
 
         tmsreader = new TmsMapReader("foo", context, tmsSpec);
 
-
-        //assertEquals("Default format (null path_format) did not get created correctly", , );
+        assertEquals("Origin X is not set (via tileOrigin.x) as expected",-10.0f,tmsreader.tileCacheLayerInfo.originX);
+        assertEquals("Origin Y is not set (via tileOrigin.y) as expected",-20.0f,tmsreader.tileCacheLayerInfo.originY);
     }
 
     public void testOriginLatLon() throws JSONException{
         JSONObject tms_full = tmsSpec.getInternalObj();
         Map<String, Float> origin = new HashMap<String, Float>();
-        origin.put("lat",-10.0f);
+        origin.put("lat",-20.0f);
         origin.put("lon",-10.0f);
 
         tms_full.accumulate("tileOrigin", new JSONObject(origin));
@@ -142,8 +141,9 @@ public class TMSLayerTest extends PrintTestCase {
 
         tmsreader = new TmsMapReader("foo", context, tmsSpec);
 
+        assertEquals("Origin X is not set (via tileOrigin.lon) as expected",-10.0f,tmsreader.tileCacheLayerInfo.originX);
+        assertEquals("Origin Y is not set (via tileOrigin.lat) as expected",-20.0f,tmsreader.tileCacheLayerInfo.originY);
 
-        //assertEquals("Default format (null path_format) did not get created correctly", , );
     }
 
     private String getBaseDir() {
