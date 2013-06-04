f4d8375524d64756a8100d64a68ac3abe831003a
jbjonesjr <jbjonesjr@gmail.com>
2013-06-03 20:51:03 -0400

    Start with the new maptestbasic

--- src/test/java/org/mapfish/print/map/readers/XYZLayerTest.java
+++ src/test/java/org/mapfish/print/map/readers/XYZLayerTest.java
@@ -7,10 +7,7 @@ import com.lowagie.text.pdf.PdfWriter;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
-import org.mapfish.print.MapPrinter;
-import org.mapfish.print.PrintTestCase;
-import org.mapfish.print.RenderingContext;
-import org.mapfish.print.ShellMapPrinter;
+import org.mapfish.print.*;
 import org.mapfish.print.config.Config;
 import org.mapfish.print.config.HostMatcher;
 import org.mapfish.print.config.layout.Block;
@@ -34,16 +31,9 @@ import java.util.*;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class XYZLayerTest extends PrintTestCase {
-
-    private static final int MARGIN = 40;
-
+public class XYZLayerTest extends MapTestBasic {
 
     private XyzMapReader xyzreader;
-    protected Document doc;
-    protected RenderingContext context;
-    private PdfWriter writer;
-    private OutputStream outFile;
 
     PJsonObject xyzSpec;
 
@@ -54,27 +44,6 @@ public class XYZLayerTest extends PrintTestCase {
 
     protected void setUp() throws Exception {
         super.setUp();
-        //  PJsonObject spec = MapPrinter.parseSpec(FileUtilities.readWholeTextFile(new File(XYZLayerTest.class.getClassLoader().getResource("samples/spec.json").getFile())));
-        // spec.getInternalObj().put("units", "meters");
-
-        doc = new Document(PageSize.A4);
-        String baseDir = getBaseDir();
-        outFile = new FileOutputStream(baseDir + getClass().getSimpleName() + "_" + getName() + ".pdf");
-        writer = PdfWriter.getInstance(doc, outFile);
-        writer.setFullCompression();
-        Layout layout = new Layout();
-        MainPage mainPage = new MainPage();
-        final MapBlock mapBlock = new MapBlock();
-        mainPage.setItems(new ArrayList<Block>(Arrays.asList(mapBlock)));
-        layout.setMainPage(mainPage);
-        Config config = new Config();
-        config.setDpis(new TreeSet<Integer>(Arrays.asList(96, 190, 254)));
-        config.setScales(new TreeSet<Integer>(Arrays.asList(20000, 25000, 100000, 500000, 4000000)));
-        List<HostMatcher> hosts = new ArrayList<HostMatcher>(1);
-        hosts.add(HostMatcher.ACCEPT_ALL);
-        config.setHosts(hosts);
-        context = new RenderingContext(doc, writer, config, null, null, layout, Collections.<String, String>emptyMap());
-
 
         xyzSpec = MapPrinter.parseSpec(FileUtilities.readWholeTextFile(
                 new File(XYZLayerTest.class.getClassLoader().getResource("layers/layer_spec.json").getFile())
@@ -83,10 +52,6 @@ public class XYZLayerTest extends PrintTestCase {
     }
 
     protected void tearDown() throws Exception {
-        writer.close();
-        outFile.close();
-
-        context = null;
 
         super.tearDown();
     }
@@ -102,14 +67,13 @@ public class XYZLayerTest extends PrintTestCase {
         xyzreader = new XyzMapReader("foo", context, xyzSpec);
 
         URI outputuri = xyzreader.getTileUri(URIUtils.addParams(xyzreader.baseUrl, new HashMap<String, List<String>>(), new MatchAllSet<String>()), null, -180, -90, 180, 90, 256, 256);
-        String url = outputuri.getScheme() +"://"+ outputuri.getHost() + outputuri.getPath();
 
-        assertEquals("Default format (null path_format) did not get created correctly", expected_url, url);
+        assertEquals("Default format (null path_format) did not get created correctly", expected_url, outputuri.toURL().toString());
     }
 
     public void testUriWithFormat() throws IOException, JSONException, URISyntaxException {
         String test_format = "${z}_${x}_${y}_static.${extension}";
-        String expected_url = xyzSpec.getString("baseURL") + "/07_64_63_static.gif";
+        String expected_url = xyzSpec.getString("baseURL") + "/7_64_63_static.gif";
 
 
         JSONObject xyz_full = xyzSpec.getInternalObj();
@@ -119,26 +83,9 @@ public class XYZLayerTest extends PrintTestCase {
         xyzreader = new XyzMapReader("foo", context, xyzSpec);
 
         URI outputuri = xyzreader.getTileUri(URIUtils.addParams(xyzreader.baseUrl, new HashMap<String, List<String>>(), new MatchAllSet<String>()), null, -180, -90, 180, 90, 256, 256);
-        String url = outputuri.getScheme() +"://"+ outputuri.getHost() + outputuri.getPath();
 
-        assertEquals("Custom format did not get created correctly", expected_url, url);
+        assertEquals("Custom format did not get created correctly", expected_url, outputuri.toURL().toString());
     }
 
-    private String getBaseDir() {
-        //This test expects to be able to write files into the same directory the classes
-        //are compiled to, in this case the build/classes/test directory
-        String expectedPath = "build" + File.separator + "classes" + File.separator + "test";
-        String baseDir = XYZLayerTest.class.getClassLoader().getResource(".").getFile();
-        if (baseDir.indexOf("pulse-java.jar") != -1) {
-            String[] paths = System.getProperty("java.class.path").split(File.pathSeparator);
-
-            for (String path : paths) {
-                if (path.indexOf(expectedPath) != -1 || path.indexOf("out/test/mapfish-print") != -1) {
-                    baseDir = path;
-                }
-            }
-        }
-        return baseDir;
-    }
 
 }
