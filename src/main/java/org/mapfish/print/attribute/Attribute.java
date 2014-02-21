edc00ef95359d4534fc50ae29d10a471a453440a
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-02-21 09:23:06 +0100

    add empty attribute and processor interfaces

---
+++ src/main/java/org/mapfish/print/attribute/Attribute.java
@@ -0,0 +1,9 @@
+package org.mapfish.print.attribute;
+
+/**
+ * Represents an attribute passed in from a web-client to be used to populate the report.
+ *
+ * Created by Jesse on 2/21/14.
+ */
+public interface Attribute {
+}
---
+++ src/main/java/org/mapfish/print/processor/Processor.java
@@ -0,0 +1,8 @@
+package org.mapfish.print.processor;
+
+/**
+ * Interface for processing input attributes
+ * Created by Jesse on 2/21/14.
+ */
+public interface Processor<T> {
+}
