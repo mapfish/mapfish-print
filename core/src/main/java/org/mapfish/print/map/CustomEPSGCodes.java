6d8b662d984c1e9f320c588712da416d7806e0fe
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-04-09 16:02:45 +0200

    Update copyright headers

--- core/src/main/java/org/mapfish/print/map/CustomEPSGCodes.java
+++ core/src/main/java/org/mapfish/print/map/CustomEPSGCodes.java
@@ -1,18 +1,20 @@
 /*
- *    GeoTools - The Open Source Java GIS Toolkit
- *    http://geotools.org
- * 
- *    (C) 2005-2008, Open Source Geospatial Foundation (OSGeo)
+ * Copyright (C) 2014  Camptocamp
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation;
- *    version 2.1 of the License.
+ * This file is part of MapFish Print
  *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
+ * MapFish Print is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MapFish Print is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MapFish Print.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.mapfish.print.map;
 
--- core/src/main/java/org/mapfish/print/processor/jasper/TableDataSource.java
+++ core/src/main/java/org/mapfish/print/processor/jasper/TableDataSource.java
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with MapFish Print.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 /**
  * @author Stéphane Brunner
  */
