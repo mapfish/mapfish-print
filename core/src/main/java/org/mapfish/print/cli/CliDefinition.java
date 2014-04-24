e916f711051fe1f90095c22ecdc20c3359ba40ec
Stéphane Brunner <stephane.brunner@camptocamp.com>
2014-04-24 10:22:04 +0200

    Int attribute isn't supported ...

--- core/src/main/java/org/mapfish/print/cli/CliDefinition.java
+++ core/src/main/java/org/mapfish/print/cli/CliDefinition.java
@@ -39,7 +39,7 @@ public final class CliDefinition {
 
     @Argument(description = "Used only if log4jConfig is not specified. 3 if you want everything, " +
                             "2 if you want the debug information (stacktraces are shown), 1 for infos and 0 for only warnings and errors")
-    public static int verbose = 1;
+    public static String verbose = "1";
 
     @Argument(description = "The destination file. By default, Standard Out")
     public static String output = null;
