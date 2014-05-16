2ecc241e299f743381291b5516db8cd1dbb63c0b
tsauerwein <tobias.sauerwein@camptocamp.com>
2014-05-16 10:02:05 +0200

    Add tests for metrics API

--- examples/src/test/java/org/mapfish/print/MetricsApiTest.java
+++ examples/src/test/java/org/mapfish/print/MetricsApiTest.java
@@ -20,13 +20,17 @@
 package org.mapfish.print;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import org.json.JSONObject;
 import org.junit.Test;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequest;
 
 /*
@@ -43,11 +47,41 @@ import org.springframework.http.client.ClientHttpRequest;
 public class MetricsApiTest extends AbstractApiTest {
 
     @Test
+    public void testMetrics() throws Exception {
+        ClientHttpRequest request = getMetricsRequest("metrics", HttpMethod.GET);
+        response = request.execute();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+        JSONObject sampleRequest = new JSONObject(getBodyAsText(response));
+        assertTrue(sampleRequest.has("version"));
+    }
+
+    @Test
     public void testPing() throws Exception {
         ClientHttpRequest request = getMetricsRequest("ping", HttpMethod.GET);
         response = request.execute();
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("pong\n", getBodyAsText(response));
+    }
+
+    @Test
+    public void testThreads() throws Exception {
+        ClientHttpRequest request = getMetricsRequest("threads", HttpMethod.GET);
+        response = request.execute();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(MediaType.TEXT_PLAIN, response.getHeaders().getContentType());
+        assertFalse(getBodyAsText(response).isEmpty());
+    }
+
+    @Test
+    public void testHealthcheck() throws Exception {
+        ClientHttpRequest request = getMetricsRequest("healthcheck", HttpMethod.GET);
+        response = request.execute();
+        // TODO not implemented?
+        assertEquals(HttpStatus.NOT_IMPLEMENTED, response.getStatusCode());
+//        assertEquals(HttpStatus.OK, response.getStatusCode());
+//        assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+//        assertNotNull(new JSONObject(getBodyAsText(response)));
     }
 
     private ClientHttpRequest getMetricsRequest(String path, HttpMethod method) throws IOException,
