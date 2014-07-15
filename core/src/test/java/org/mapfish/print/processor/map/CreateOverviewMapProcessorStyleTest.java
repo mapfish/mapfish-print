2c694b288843471d58f6a0f175102c6b23f2418b
tsauerwein <tobias.sauerwein@camptocamp.com>
2014-07-15 16:07:56 +0200

    Add test with custom style for the bbox rectangle

---
+++ core/src/test/java/org/mapfish/print/processor/map/CreateOverviewMapProcessorStyleTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2014  Camptocamp
+ *
+ * This file is part of MapFish Print
+ *
+ * MapFish Print is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MapFish Print is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MapFish Print.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mapfish.print.processor.map;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+
+import org.junit.Test;
+import org.mapfish.print.AbstractMapfishSpringTest;
+import org.mapfish.print.TestHttpClientFactory;
+import org.mapfish.print.config.Configuration;
+import org.mapfish.print.config.ConfigurationFactory;
+import org.mapfish.print.config.Template;
+import org.mapfish.print.output.Values;
+import org.mapfish.print.parser.MapfishParser;
+import org.mapfish.print.test.util.ImageSimilarity;
+import org.mapfish.print.wrapper.json.PJsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.mock.http.client.MockClientHttpRequest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import com.google.common.base.Predicate;
+import com.google.common.io.Files;
+
+/**
+ * Test of the CreateOverviewMap processor with a custom style for the bbox rectangle.
+ */
+public class CreateOverviewMapProcessorStyleTest extends AbstractMapfishSpringTest {
+    public static final String BASE_DIR = "overview_map_style/";
+
+    @Autowired
+    private ConfigurationFactory configurationFactory;
+    @Autowired
+    private TestHttpClientFactory requestFactory;
+    @Autowired
+    private MapfishParser parser;
+
+    @Test
+    @DirtiesContext
+    public void testExecute() throws Exception {
+        final String host = "overview_map_style";
+        requestFactory.registerHandler(
+                new Predicate<URI>() {
+                    @Override
+                    public boolean apply(URI input) {
+                        return (("" + input.getHost()).contains(host + ".osm")) || input.getAuthority().contains(host + ".osm");
+                    }
+                }, new TestHttpClientFactory.Handler() {
+                    @Override
+                    public MockClientHttpRequest handleRequest(URI uri, HttpMethod httpMethod) throws Exception {
+                        try {
+                            byte[] bytes = Files.toByteArray(getFile("/map-data/osm" + uri.getPath()));
+                            return ok(uri, bytes, httpMethod);
+                        } catch (AssertionError e) {
+                            return error404(uri, httpMethod);
+                        }
+                    }
+                }
+        );
+        requestFactory.registerHandler(
+                new Predicate<URI>() {
+                    @Override
+                    public boolean apply(URI input) {
+                        return (("" + input.getHost()).contains(host + ".json")) || input.getAuthority().contains(host + ".json");
+                    }
+                }, new TestHttpClientFactory.Handler() {
+                    @Override
+                    public MockClientHttpRequest handleRequest(URI uri, HttpMethod httpMethod) throws Exception {
+                        try {
+                            byte[] bytes = Files.toByteArray(getFile("/map-data" + uri.getPath()));
+                            return ok(uri, bytes, httpMethod);
+                        } catch (AssertionError e) {
+                            return error404(uri, httpMethod);
+                        }
+                    }
+                }
+        );
+        final Configuration config = configurationFactory.getConfig(getFile(BASE_DIR + "config.yaml"));
+        final Template template = config.getTemplate("main");
+        PJsonObject requestData = loadJsonRequestData();
+        Values values = new Values(requestData, template, this.parser, getTaskDirectory(), this.requestFactory);
+        template.getProcessorGraph().createTask(values).invoke();
+
+        @SuppressWarnings("unchecked")
+        List<URI> layerGraphics = (List<URI>) values.getObject("overviewMapLayerGraphics", List.class);
+        assertEquals(3, layerGraphics.size());
+
+//        Files.copy(new File(layerGraphics.get(0)), new File("/tmp/0_ov_"+getClass().getSimpleName()+".tiff"));
+//        Files.copy(new File(layerGraphics.get(1)), new File("/tmp/1_ov_"+getClass().getSimpleName()+".tiff"));
+//        Files.copy(new File(layerGraphics.get(2)), new File("/tmp/2_ov_"+getClass().getSimpleName()+".tiff"));
+        
+        new ImageSimilarity(ImageSimilarity.mergeImages(layerGraphics, 300, 200), 2)
+                .assertSimilarity(getFile(BASE_DIR + "expectedSimpleImage.tiff"), 30);
+
+    }
+
+    private static PJsonObject loadJsonRequestData() throws IOException {
+        return parseJSONObjectFromFile(CreateOverviewMapProcessorStyleTest.class, BASE_DIR + "requestData.json");
+    }
+
+}
---
+++ core/src/test/resources/org/mapfish/print/processor/map/overview_map_style/overview-map.sld
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0"
+    xsi:schemaLocation="http://www.opengis.net/sld StyledLayerDescriptor.xsd"
+    xmlns="http://www.opengis.net/sld"
+    xmlns:ogc="http://www.opengis.net/ogc"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <NamedLayer>
+        <Name>overview-map</Name>
+        <UserStyle>
+            <FeatureTypeStyle>
+                <Rule>
+                    <Name>rule1</Name>
+                    <PolygonSymbolizer>
+                     <Fill>
+                        <CssParameter name="fill">#009933</CssParameter>
+                        <CssParameter name="fill-opacity">0.3</CssParameter>
+                     </Fill>
+                     <Stroke>
+                        <CssParameter name="stroke">#009933</CssParameter>
+                        <CssParameter name="stroke-width">1</CssParameter>
+                        <CssParameter name="stroke-dasharray">5 2</CssParameter>
+                     </Stroke>
+                    </PolygonSymbolizer>
+                </Rule>
+            </FeatureTypeStyle>
+        </UserStyle>
+    </NamedLayer>
+</StyledLayerDescriptor>
+
