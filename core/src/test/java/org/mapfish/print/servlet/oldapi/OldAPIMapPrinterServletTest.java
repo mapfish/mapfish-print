aef734edd9525ee03cce3ba17b63f82e61f7a77f
tsauerwein <tobias.sauerwein@camptocamp.com>
2014-07-16 17:02:44 +0200

    Output scales and dpi values in `info.json` of old api

--- core/src/test/java/org/mapfish/print/servlet/oldapi/OldAPIMapPrinterServletTest.java
+++ core/src/test/java/org/mapfish/print/servlet/oldapi/OldAPIMapPrinterServletTest.java
@@ -74,6 +74,19 @@ public class OldAPIMapPrinterServletTest extends AbstractMapfishSpringTest {
         assertTrue(info.has("layouts"));
         assertTrue(info.has("printURL"));
         assertTrue(info.has("createURL"));
+
+        assertEquals(7, info.getArray("scales").size());
+        final PObject firstScale = info.getArray("scales").getObject(0);
+        assertEquals("1:5000", firstScale.getString("name"));
+        assertEquals("5000", firstScale.getString("value"));
+
+        assertEquals(6, info.getArray("dpis").size());
+        final PObject firstDpi = info.getArray("dpis").getObject(0);
+        assertEquals("72", firstDpi.getString("name"));
+        assertEquals("72", firstDpi.getString("value"));
+        final PObject lastDpi = info.getArray("dpis").getObject(5);
+        assertEquals("400", lastDpi.getString("name"));
+        assertEquals("400", lastDpi.getString("value"));
         
         assertTrue(info.getArray("outputFormats").size() > 0);
         assertTrue(info.getArray("outputFormats").getObject(0).has("name"));
