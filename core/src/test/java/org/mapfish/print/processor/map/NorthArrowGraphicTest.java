9746e442a805d111144231366b247c35f52edbe3
tsauerwein <tobias.sauerwein@camptocamp.com>
2014-08-12 15:36:07 +0200

    Move test to the right place

--- core/src/test/java/org/mapfish/print/processor/map/NorthArrowGraphicTest.java
+++ core/src/test/java/org/mapfish/print/processor/map/NorthArrowGraphicTest.java
@@ -17,7 +17,7 @@
  * along with MapFish Print.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.mapfish.print.processor.map.north_arrow;
+package org.mapfish.print.processor.map;
 
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
@@ -38,6 +38,8 @@ import org.mapfish.print.test.util.ImageSimilarity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class NorthArrowGraphicTest extends AbstractMapfishSpringTest {
+    private static final String BASE_DIR = "north_arrow/";
+
     @Autowired
     private ConfigurationFactory configurationFactory;
 
@@ -48,119 +50,119 @@ public class NorthArrowGraphicTest extends AbstractMapfishSpringTest {
 
     @Before
     public void setup() throws IOException {
-        this.config = this.configurationFactory.getConfig(getFile("config.yaml"));
+        this.config = this.configurationFactory.getConfig(getFile(BASE_DIR + "config.yaml"));
     }
 
     @Test
     public void testCreatePngSquareDownScaled() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(200, 200), "file://" + getFile("NorthArrow.png").toString(), 0.0,
+                new Dimension(200, 200), "file://" + getFile(BASE_DIR + "NorthArrow.png").toString(), 0.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.tiff"));
 //        ImageSimilarity.writeUncompressedImage(ImageIO.read(new File(file)), "/tmp/expected-north-arrow-png-square-down.tiff");
-        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile("expected-north-arrow-png-square-down.tiff"), 0);
+        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow-png-square-down.tiff"), 0);
     }
 
     @Test
     public void testCreatePngSquareUpScaled() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(50, 50), "file://" + getFile("NorthArrow.png").toString(), 0.0,
+                new Dimension(50, 50), "file://" + getFile(BASE_DIR + "NorthArrow.png").toString(), 0.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.tiff"));
 //        ImageSimilarity.writeUncompressedImage(ImageIO.read(new File(file)), "/tmp/expected-north-arrow-png-square-up.tiff");
-        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile("expected-north-arrow-png-square-up.tiff"), 0);
+        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow-png-square-up.tiff"), 0);
     }
 
     @Test
     public void testCreatePngSquareRotatedDownScaled() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(200, 200), "file://" + getFile("NorthArrow.png").toString(), 45.0,
+                new Dimension(200, 200), "file://" + getFile(BASE_DIR + "NorthArrow.png").toString(), 45.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.tiff"));
 //        ImageSimilarity.writeUncompressedImage(ImageIO.read(new File(file)), "/tmp/expected-north-arrow-png-square-45-down.tiff");
-        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile("expected-north-arrow-png-square-45-down.tiff"), 0);
+        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow-png-square-45-down.tiff"), 0);
     }
 
     @Test
     public void testCreatePngSquareRotatedUpScaled() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(50, 50), "file://" + getFile("NorthArrow.png").toString(), 45.0,
+                new Dimension(50, 50), "file://" + getFile(BASE_DIR + "NorthArrow.png").toString(), 45.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.tiff"));
 //        ImageSimilarity.writeUncompressedImage(ImageIO.read(new File(file)), "/tmp/expected-north-arrow-png-square-45-up.tiff");
-        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile("expected-north-arrow-png-square-45-up.tiff"), 0);
+        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow-png-square-45-up.tiff"), 0);
     }
 
     @Test
     public void testCreatePngNoSquareDownScaled() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(200, 200), "file://" + getFile("NorthArrow_10.png").toString(), 0.0,
+                new Dimension(200, 200), "file://" + getFile(BASE_DIR + "NorthArrow_10.png").toString(), 0.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.tiff"));
 //        ImageSimilarity.writeUncompressedImage(ImageIO.read(new File(file)), "/tmp/expected-north-arrow_10-png-nosquare-down.tiff");
-        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile("expected-north-arrow_10-png-nosquare-down.tiff"), 0);
+        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow_10-png-nosquare-down.tiff"), 0);
     }
 
     @Test
     public void testCreatePngNoSquareUpScaled() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(50, 50), "file://" + getFile("NorthArrow_10.png").toString(), 0.0,
+                new Dimension(50, 50), "file://" + getFile(BASE_DIR + "NorthArrow_10.png").toString(), 0.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.tiff"));
 //        ImageSimilarity.writeUncompressedImage(ImageIO.read(new File(file)), "/tmp/expected-north-arrow_10-png-nosquare-up.tiff");
-        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile("expected-north-arrow_10-png-nosquare-up.tiff"), 0);
+        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow_10-png-nosquare-up.tiff"), 0);
     }
 
     @Test
     public void testCreatePngNoSquareRotatedDownScaled() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(200, 200), "file://" + getFile("NorthArrow_10.png").toString(), 45.0,
+                new Dimension(200, 200), "file://" + getFile(BASE_DIR + "NorthArrow_10.png").toString(), 45.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.tiff"));
 //        ImageSimilarity.writeUncompressedImage(ImageIO.read(new File(file)), "/tmp/expected-north-arrow_10-png-nosquare-45-down.tiff");
-        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile("expected-north-arrow_10-png-nosquare-45-down.tiff"), 0);
+        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow_10-png-nosquare-45-down.tiff"), 0);
     }
 
     @Test
     public void testCreatePngNoSquareRotatedUpScaled() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(50, 50), "file://" + getFile("NorthArrow_10.png").toString(), 45.0,
+                new Dimension(50, 50), "file://" + getFile(BASE_DIR + "NorthArrow_10.png").toString(), 45.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.tiff"));
 //        ImageSimilarity.writeUncompressedImage(ImageIO.read(new File(file)), "/tmp/expected-north-arrow_10-png-nosquare-45-up.tiff");
-        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile("expected-north-arrow_10-png-nosquare-45-up.tiff"), 0);
+        new ImageSimilarity(new File(file), 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow_10-png-nosquare-45-up.tiff"), 0);
     }
 
     @Test
     public void testCreateSvgWidthAndHeightSet() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(200, 200), "file://" + getFile("NorthArrow.svg").toString(), 90.0,
+                new Dimension(200, 200), "file://" + getFile(BASE_DIR + "NorthArrow.svg").toString(), 90.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
         BufferedImage referenceImage = ImageSimilarity.convertFromSvg(file, 200, 200);
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow.svg"));
 //        ImageSimilarity.writeUncompressedImage(referenceImage, "/tmp/expected-north-arrow.tiff");
-        new ImageSimilarity(referenceImage, 2).assertSimilarity(getFile("expected-north-arrow.tiff"), 0);
+        new ImageSimilarity(referenceImage, 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow.tiff"), 0);
     }
 
     @Test
     public void testCreateSvgWidthAndHeightNotSet() throws Exception {
         URI file = NorthArrowGraphic.create(
-                new Dimension(200, 200), "file://" + getFile("NorthArrow_10.svg").toString(), 90.0,
+                new Dimension(200, 200), "file://" + getFile(BASE_DIR + "NorthArrow_10.svg").toString(), 90.0,
                 getTaskDirectory(), getGraphicLoader(), requestFactory);
 
         BufferedImage referenceImage = ImageSimilarity.convertFromSvg(file, 200, 200);
 //        FileUtils.copyFile(new File(file), new File("/tmp/north-arrow_10.svg"));
 //        ImageSimilarity.writeUncompressedImage(referenceImage, "/tmp/expected-north-arrow_10.tiff");
-        new ImageSimilarity(referenceImage, 2).assertSimilarity(getFile("expected-north-arrow_10.tiff"), 0);
+        new ImageSimilarity(referenceImage, 2).assertSimilarity(getFile(BASE_DIR + "expected-north-arrow_10.tiff"), 0);
     }
 
     private GraphicLoader getGraphicLoader() {
