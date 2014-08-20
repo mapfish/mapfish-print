4fba4669c40791f5418754506e685b513ce158e2
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-08-20 11:23:23 +0200

    Fix test name for WMX test

--- core/src/test/java/org/mapfish/print/map/tiled/AbstractWMXLayerParamsTest.java
+++ core/src/test/java/org/mapfish/print/map/tiled/AbstractWMXLayerParamsTest.java
@@ -30,7 +30,7 @@ import static org.junit.Assert.assertTrue;
  * Test param methods.
  * @author Jesse on 4/4/14.
  */
-public class AbstractTiledLayerParamsTest {
+public class AbstractWMXLayerParamsTest {
     private static final String CUSTOM_PARAMS = "{\"key\": \"value\", \"key2\":[\"value1\", \"value2\"], \"key3\": null}";
     @Test
     public void testCustomParams() throws Exception {
