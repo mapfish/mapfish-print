4431919db6bfe0f89a0de0982d693d62ea201eb4
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-11-07 09:58:25 +0100

    add opacity test

---
+++ core/src/test/java/org/mapfish/print/processor/map/CreateMapProcessorOpacityWMTSTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2014  Camptocamp
+ *
+ * This file is part of MapFish Print
+ *
+ * MapFish Print is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MapFish Print is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MapFish Print.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mapfish.print.processor.map;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Multimap;
+import com.google.common.io.Files;
+import org.junit.Test;
+import org.mapfish.print.AbstractMapfishSpringTest;
+import org.mapfish.print.TestHttpClientFactory;
+import org.mapfish.print.URIUtils;
+import org.mapfish.print.config.Configuration;
+import org.mapfish.print.config.ConfigurationFactory;
+import org.mapfish.print.config.Template;
+import org.mapfish.print.output.Values;
+import org.mapfish.print.parser.MapfishParser;
+import org.mapfish.print.test.util.ImageSimilarity;
+import org.mapfish.print.wrapper.json.PJsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.mock.http.client.MockClientHttpRequest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Basic test of the Map processor.
+ * <p/>
+ * Created by Jesse on 3/26/14.
+ */
+public class CreateMapProcessorOpacityWMTSTest extends AbstractMapfishSpringTest {
+    public static final String BASE_DIR = "opacity_center_wmts_fixedscale/";
+
+    @Autowired
+    private ConfigurationFactory configurationFactory;
+    @Autowired
+    private TestHttpClientFactory requestFactory;
+    @Autowired
+    private MapfishParser parser;
+
+
+    @Test
+    @DirtiesContext
+    public void testExecute() throws Exception {
+        requestFactory.registerHandler(
+                new Predicate<URI>() {
+                    @Override
+                    public boolean apply(URI input) {
+                        final String host = "center_wmts_fixedscale.com";
+                        return (("" + input.getHost()).contains(host)) || input.getAuthority().contains(host);
+                    }
+                }, new TestHttpClientFactory.Handler() {
+                    @Override
+                    public MockClientHttpRequest handleRequest(URI uri, HttpMethod httpMethod) throws Exception {
+                        final Multimap<String, String> parameters = URIUtils.getParameters(uri);
+                        String column = parameters.get("TILECOL").iterator().next();
+                        String row = parameters.get("TILEROW").iterator().next();
+                        try {
+                            byte[] bytes = Files.toByteArray(getFile("/map-data/ny-tiles/" + column + "x" + row + "" +
+
+                                                                     ".tiff"));
+                            return ok(uri, bytes, httpMethod);
+                        } catch (AssertionError e) {
+                            return error404(uri, httpMethod);
+                        }
+                    }
+                }
+        );
+        requestFactory.registerHandler(
+                new Predicate<URI>() {
+                    @Override
+                    public boolean apply(URI input) {
+                        final String host = "center_wmts_fixedscale.json";
+                        return (("" + input.getHost()).contains(host)) || input.getAuthority().contains(host);
+                    }
+                }, new TestHttpClientFactory.Handler() {
+                    @Override
+                    public MockClientHttpRequest handleRequest(URI uri, HttpMethod httpMethod) throws Exception {
+                        try {
+                            byte[] bytes = Files.toByteArray(getFile("/map-data" + uri.getPath()));
+                            return ok(uri, bytes, httpMethod);
+                        } catch (AssertionError e) {
+                            return error404(uri, httpMethod);
+                        }
+                    }
+                }
+        );
+
+        final Configuration config = configurationFactory.getConfig(getFile(BASE_DIR + "config.yaml"));
+        final Template template = config.getTemplate("main");
+        PJsonObject requestData = loadJsonRequestData();
+        Values values = new Values(requestData, template, parser, getTaskDirectory(), this.requestFactory, new File("."));
+        template.getProcessorGraph().createTask(values).invoke();
+
+        @SuppressWarnings("unchecked")
+        List<URI> layerGraphics = (List<URI>) values.getObject("layerGraphics", List.class);
+        assertEquals(3, layerGraphics.size());
+
+        final BufferedImage referenceImage = ImageSimilarity.mergeImages(layerGraphics, 630, 294);
+//        ImageIO.write(referenceImage, "tif", new File("e:/tmp/expectedSimpleImage.tiff"));
+        new ImageSimilarity(referenceImage, 2)
+                .assertSimilarity(getFile(BASE_DIR + "expectedSimpleImage.tiff"), 20);
+    }
+
+    public static PJsonObject loadJsonRequestData() throws IOException {
+        return parseJSONObjectFromFile(CreateMapProcessorOpacityWMTSTest.class, BASE_DIR + "requestData.json");
+    }
+
+}
---
+++ core/src/test/resources/org/mapfish/print/processor/map/opacity_center_wmts_fixedscale/thickline.sld
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0" xsi:schemaLocation="http://www.opengis.net/sld StyledLayerDescriptor.xsd"
+    xmlns="http://www.opengis.net/sld" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <!-- a Named Layer is the basic building block of an SLD document -->
+    <NamedLayer>
+        <Name>default_line</Name>
+        <UserStyle>
+            <!-- Styles can have names, titles and abstracts -->
+            <Title>Default Line</Title>
+            <Abstract>A sample style that draws a line</Abstract>
+            <!-- FeatureTypeStyles describe how to render different features -->
+            <!-- A FeatureTypeStyle for rendering lines -->
+            <FeatureTypeStyle>
+                <Rule>
+                    <Name>rule1</Name>
+                    <Title>Black Line</Title>
+                    <Abstract>A solid black line with a 5 pixel width</Abstract>
+                    <LineSymbolizer>
+                        <Stroke>
+                            <CssParameter name="stroke">#000000</CssParameter>
+                            <CssParameter name="stroke-width">5</CssParameter>
+                        </Stroke>
+                    </LineSymbolizer>
+                </Rule>
+            </FeatureTypeStyle>
+        </UserStyle>
+    </NamedLayer>
+</StyledLayerDescriptor>
+
