3686864716ed805772e2bff0b8f5228a46a7b0a2
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-07-09 10:00:20 +0200

    Don't draw separator line if single page

--- core/src/main/java/org/mapfish/print/output/JasperReportImageOutputFormat.java
+++ core/src/main/java/org/mapfish/print/output/JasperReportImageOutputFormat.java
@@ -23,12 +23,15 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperPrintManager;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Stroke;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
+
 import javax.imageio.ImageIO;
 
 /**
@@ -63,9 +66,14 @@ public final class JasperReportImageOutputFormat extends AbstractJasperReportOut
         final int numPages = jasperPrint.getPages().size();
 
         final float dpiRatio = (float) (print.dpi / print.requestorDpi);
-        final int pageHeightOnImage = (int) ((jasperPrint.getPageHeight() + 1) * dpiRatio);
+        final int pageHeightOnImage = (int) (jasperPrint.getPageHeight() * dpiRatio);
         final int pageWidthOnImage = (int) (jasperPrint.getPageWidth() * dpiRatio);
-        BufferedImage reportImage = new BufferedImage(pageWidthOnImage, numPages * pageHeightOnImage, this.imageType);
+        final int separatorHeight = 1;
+        final int separatorHeightOnImage = (int) (separatorHeight * dpiRatio);
+
+        BufferedImage reportImage = new BufferedImage(
+                pageWidthOnImage, numPages * pageHeightOnImage + (numPages - 1) * separatorHeightOnImage,
+                this.imageType);
 
         Graphics2D graphics2D = reportImage.createGraphics();
         try {
@@ -73,10 +81,23 @@ public final class JasperReportImageOutputFormat extends AbstractJasperReportOut
 
             for (int pageIndex = 0; pageIndex < numPages; pageIndex++) {
                 Image pageImage = printManager.printToImage(jasperPrint, pageIndex, dpiRatio);
-                graphics2D.drawImage(pageImage, 0, pageHeightOnImage * pageIndex, null);
 
+                graphics2D.drawImage(pageImage,
+                        0, (pageHeightOnImage + separatorHeight) * pageIndex,
+                        pageWidthOnImage, (pageHeightOnImage + separatorHeight) * pageIndex + pageHeightOnImage,
+                        0, 0,
+                        pageWidthOnImage, pageHeightOnImage, null);
+            }
+
+            // draw separator line between the pages
+            final Stroke stroke = new BasicStroke(separatorHeightOnImage);
+            for (int pageIndex = 0; pageIndex < numPages - 1; pageIndex++) {
                 graphics2D.setColor(Color.black);
-                graphics2D.drawLine(0, pageHeightOnImage, pageWidthOnImage, pageHeightOnImage);
+                graphics2D.setStroke(stroke);
+                int y = (pageHeightOnImage + separatorHeight) * pageIndex + pageHeightOnImage;
+                graphics2D.drawLine(
+                        0, y,
+                        pageWidthOnImage, y);
             }
         } finally {
             graphics2D.dispose();
