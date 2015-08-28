3dd0642154478e8791bc0a3402c6430830a2d92b
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-08-28 15:42:45 +0200

    Add test for scalebar default value

--- core/src/test/java/org/mapfish/print/attribute/ScalebarAttributeTest.java
+++ core/src/test/java/org/mapfish/print/attribute/ScalebarAttributeTest.java
@@ -1,16 +1,33 @@
 package org.mapfish.print.attribute;
 
 import com.google.common.collect.Lists;
+
 import org.junit.Test;
+import org.mapfish.print.AbstractMapfishSpringTest;
+import org.mapfish.print.TestHttpClientFactory;
 import org.mapfish.print.attribute.ScalebarAttribute.ScalebarAttributeValues;
 import org.mapfish.print.config.Configuration;
+import org.mapfish.print.config.ConfigurationFactory;
+import org.mapfish.print.config.Template;
+import org.mapfish.print.output.Values;
+import org.mapfish.print.parser.MapfishParser;
+import org.mapfish.print.processor.map.scalebar.Type;
+import org.mapfish.print.wrapper.json.PJsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.awt.Dimension;
+import java.io.File;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-public class ScalebarAttributeTest {
+public class ScalebarAttributeTest extends AbstractMapfishSpringTest {
+
+    @Autowired
+    private ConfigurationFactory configurationFactory;
+    @Autowired
+    private TestHttpClientFactory httpRequestFactory;
 
     @Test
     public void testValidate() {
@@ -57,6 +74,24 @@ public class ScalebarAttributeTest {
 
         values.intervals = 1;
         values.postConstruct();
+    }
+
+    @Test
+    public void testAttributesFromJson() throws Exception {
+        final File configFile = getFile(ScalebarAttributeTest.class, "scalebar/config.yaml");
+        final Configuration config = configurationFactory.getConfig(configFile);
+        final Template template = config.getTemplate("main");
+        final PJsonObject pJsonObject = parseJSONObjectFromFile(ScalebarAttributeTest.class, "scalebar/requestData.json");
+        final Values values = new Values(pJsonObject, template, new MapfishParser(), getTaskDirectory(), this.httpRequestFactory,
+                new File("."));
+        final ScalebarAttribute.ScalebarAttributeValues value = values.getObject("scalebar", ScalebarAttribute.ScalebarAttributeValues.class);
+
+        assertEquals(Type.LINE.getLabel(), value.type);
+        assertEquals("m", value.unit);
+        assertTrue(value.geodetic);
+        assertTrue(value.lockUnits);
+        assertEquals("Arial", value.font);
+        assertEquals("#cccccc", value.fontColor);
     }
 
     private ScalebarAttributeValues getValues() {
