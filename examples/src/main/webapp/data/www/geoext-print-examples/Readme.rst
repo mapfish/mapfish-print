3218f07d179d060b689e807b61212beae2fd0b88
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-09-01 08:46:00 +0200

    Remove outdated GeoExt examples

--- examples/src/main/webapp/data/www/geoext-print-examples/Readme.rst
+++
@@ -1,2 +0,0 @@
-The YAML configuration file for these examples can be found here:
-examples/src/test/resources/examples/geoext/config.yaml
--- examples/src/main/webapp/data/www/geoext-print-examples/js/loader.js
+++
@@ -1,8 +0,0 @@
-Ext.Loader.setConfig({
-    enabled: true,
-    disableCaching: false,
-    paths: {
-        GeoExt: '../js/geoext2/src/GeoExt',
-        Ext: "http://cdn.sencha.com/ext/gpl/4.2.1/src"
-    }
-});
