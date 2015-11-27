de3fbe348a5869080ee9963833d051296d07eeb7
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2015-11-27 08:04:52 +0100

    Add the generation of a Docker image.
    The fact that we enable Docker in Travis changes the Ubuntu version used
    12.04 -> 14.04(). This forced a small change in force_install.
    
    The openjdk7 used by this new Ubuntu has a crash when the local hostname is
    too long. Travis has huge hostnames and we have to change it to avoid the
    crash of the JVM.
    
    I've removed the Travis test on openjdk6 because it's not supported by
    graddle-docker. Shouldn't be a problem since we still build 1.6 compatible
    byte code. Nobody should use 1.6 anyway.

---
+++ travis/publish.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+# Setup login
+openssl aes-256-cbc -K $encrypted_fb15e83f0f9f_key -iv $encrypted_fb15e83f0f9f_iv \
+    -in .dockercfg.enc -out ~/.dockercfg -d
+
+
+JDK="oraclejdk7"   # what JDK we use to build the WAR that goes in the docker image
+NAME="camptocamp/mapfish_print"
+
+function publish {
+    local version=$1
+    export DOCKER_VERSION=`echo "${version}" | sed -e 's/^release\///' | sed -e 's/\//_/g'`
+
+    echo "Deploying image to docker hub for tag ${DOCKER_VERSION}"
+    ./gradlew --console=plain createDocker
+    docker push "${NAME}:${DOCKER_VERSION}"
+}
+
+if [ "${TRAVIS_JDK_VERSION}" != "${JDK}" ]
+then
+    echo "Only publishing with ${JDK}"
+    exit 0
+fi
+
+if [ "${TRAVIS_PULL_REQUEST}" == "true" ]
+then
+    echo "Not deploying image for pull requests"
+    exit 0
+fi
+
+if [ "${TRAVIS_BRANCH}" == "development" ] || [ "${TRAVIS_BRANCH}" == "docker" ]
+then
+  publish latest
+elif [ ! -z "${TRAVIS_TAG}" ]
+then
+  publish "${TRAVIS_TAG}"
+#elif [ ! -z "${TRAVIS_BRANCH}" ]
+#then
+#  publish "${TRAVIS_BRANCH}"
+else
+  echo "Not deploying image"
+fi
