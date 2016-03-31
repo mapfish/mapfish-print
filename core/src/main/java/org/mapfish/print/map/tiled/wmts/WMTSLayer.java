0ef26fcc1cb954b91d9b737af61b2043fee9f419
tsauerwein <tobias.sauerwein@camptocamp.com>
2016-03-31 17:20:11 +0200

    Download tiles in parallel

--- core/src/main/java/org/mapfish/print/map/tiled/wmts/WMTSLayer.java
+++ core/src/main/java/org/mapfish/print/map/tiled/wmts/WMTSLayer.java
@@ -39,16 +39,18 @@ public class WMTSLayer extends AbstractTiledLayer {
     /**
      * Constructor.
      *
-     * @param executorService the thread pool for doing the rendering.
+     * @param forkJoinPool the thread pool for doing the rendering.
+     * @param requestForkJoinPool the thread pool for making tile/image requests.
      * @param styleSupplier   strategy for loading the style for this layer
      * @param param           the information needed to create WMTS requests.
      * @param registry      the metrics registry.
      */
-    protected WMTSLayer(final ForkJoinPool executorService,
+    protected WMTSLayer(final ForkJoinPool forkJoinPool,
+                        final ForkJoinPool requestForkJoinPool,
                         final StyleSupplier<GridCoverage2D> styleSupplier,
                         final WMTSLayerParam param,
                         final MetricRegistry registry) {
-        super(executorService, styleSupplier, param, registry);
+        super(forkJoinPool, requestForkJoinPool, styleSupplier, param, registry);
         this.param = param;
     }
 
