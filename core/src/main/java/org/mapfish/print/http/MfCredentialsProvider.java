3eeeaf992c1a2942310229c58a1330cb3f116196
Julien Acroute <julien.acroute@camptocamp.com>
2016-10-07 10:27:03 +0200

    Fix typo in javadoc

--- core/src/main/java/org/mapfish/print/http/MfCredentialsProvider.java
+++ core/src/main/java/org/mapfish/print/http/MfCredentialsProvider.java
@@ -14,7 +14,7 @@ import java.util.List;
  * A Route planner that obtains credentials from the configuration that is currently in
  * {@link org.mapfish.print.http.MfClientHttpRequestFactoryImpl#CURRENT_CONFIGURATION}.
  * <p></p>
- * If authentication is not found in configuratin then it will fall back to
+ * If authentication is not found in configuration then it will fall back to
  * {@link org.apache.http.impl.client.SystemDefaultCredentialsProvider}
  * <p></p>
  * {@link MfClientHttpRequestFactoryImpl.Request} will set the correct configuration
