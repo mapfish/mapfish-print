4b2fdd467679117a53e6f39ca0b25806d3920493
MaxComse <mkondakov@er76.ru>
2016-11-09 10:14:40 +0300

    Unit test and example for image type layer

---
+++ core/src/test/java/org/mapfish/print/processor/map/CreateMapProcessorImageLayerTest1.java
@@ -0,0 +1,84 @@
+package org.mapfish.print.processor.map;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+
+import org.junit.Test;
+import org.mapfish.print.AbstractMapfishSpringTest;
+import org.mapfish.print.TestHttpClientFactory;
+import org.mapfish.print.config.Configuration;
+import org.mapfish.print.config.ConfigurationFactory;
+import org.mapfish.print.config.Template;
+import org.mapfish.print.output.Values;
+import org.mapfish.print.parser.MapfishParser;
+import org.mapfish.print.test.util.ImageSimilarity;
+import org.mapfish.print.wrapper.json.PJsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.mock.http.client.MockClientHttpRequest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import com.google.common.base.Predicate;
+import com.google.common.io.Files;
+
+/**
+ * Basic test of the Map processor.
+ * <p></p>
+ * Created by MaxComse on 11/09/16.
+ */
+public class CreateMapProcessorImageLayerTest1 extends AbstractMapfishSpringTest {
+    public static final String BASE_DIR = "image_layer_test/";
+
+    @Autowired
+    private ConfigurationFactory configurationFactory;
+    @Autowired
+    private TestHttpClientFactory requestFactory;
+    @Autowired
+    private MapfishParser parser;
+
+    @Test
+    @DirtiesContext
+    public void testExecute() throws Exception {
+        final String host = "image_layer_test";
+        requestFactory.registerHandler(
+                new Predicate<URI>() {
+                    @Override
+                    public boolean apply(URI input) {
+                        return ("" + input.getHost()).contains(host) || input.getAuthority().contains(host);
+                    }
+                }, new TestHttpClientFactory.Handler() {
+                    @Override
+                    public MockClientHttpRequest handleRequest(URI uri, HttpMethod httpMethod) throws Exception {
+                        try {
+                            byte[] bytes = Files.toByteArray(getFile("/map-data/tiger-ny.tiff"));
+                            return ok(uri, bytes, httpMethod);
+                        } catch (AssertionError e) {
+                            return error404(uri, httpMethod);
+                        }
+                    }
+                }
+        );
+        
+        final Configuration config = configurationFactory.getConfig(getFile(BASE_DIR + "config.yaml"));
+        final Template template = config.getTemplate("main");
+        PJsonObject requestData = loadJsonRequestData();
+        Values values = new Values(requestData, template, this.parser, getTaskDirectory(), this.requestFactory, new File("."));
+        template.getProcessorGraph().createTask(values).invoke();
+
+        @SuppressWarnings("unchecked")
+        List<URI> layerGraphics = (List<URI>) values.getObject("layerGraphics", List.class);
+        assertEquals(1, layerGraphics.size());
+
+        new ImageSimilarity(ImageSimilarity.mergeImages(layerGraphics, 630, 294), 2)
+                .assertSimilarity(getFile(BASE_DIR + "expectedSimpleImage.png"), 100);
+
+    }
+
+    private static PJsonObject loadJsonRequestData() throws IOException {
+        return parseJSONObjectFromFile(CreateMapProcessorImageLayerTest1.class, BASE_DIR + "requestData.json");
+    }
+}
