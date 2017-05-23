465cc763543b7c63ee23f40bfed51c24ac1f1621
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2017-05-23 15:49:50 +0200

    use jdk7 compatible base64 decoder

--- core/src/main/java/org/mapfish/print/url/data/DataUrlConnection.java
+++ core/src/main/java/org/mapfish/print/url/data/DataUrlConnection.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Base64;
-import java.util.Base64.Decoder;
+
+import org.geotools.data.Base64;
 
 /**
  * Decodes the base64 data and provides an appropriate InputStream.
@@ -29,10 +29,9 @@ public class DataUrlConnection extends URLConnection {
 
     @Override
     public InputStream getInputStream() {
-        Decoder decoder = Base64.getDecoder();
         String url = this.url.toExternalForm();
         url = url.substring(url.lastIndexOf(",") + 1);
-        return new ByteArrayInputStream(decoder.decode(url));
+        return new ByteArrayInputStream(Base64.decode(url));
     }
 
 }
