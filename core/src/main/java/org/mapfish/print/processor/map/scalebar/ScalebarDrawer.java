mapfish/mapfish-print

commit ad0a40e8ceabf014f4fff45dee68995fea5ce342

Use radian internally

core/src/main/java/org/mapfish/print/processor/map/scalebar/ScalebarDrawer.java
@@ -153,7 +153,7 @@ public abstract class ScalebarDrawer {
     protected abstract void drawBar();
 
     private void drawLabels(final AffineTransform transform, final Orientation orientation,
-            final double labelRotation) {
+            final float labelRotation) {
         float prevMargin = orientation.isHorizontal() ? this.settings.getMaxSize().width :
                 this.settings.getMaxSize().height;
 

core/src/main/java/org/mapfish/print/attribute/ScalebarAttribute.java
@@ -245,7 +245,7 @@ public class ScalebarAttribute extends ReflectiveAttribute<ScalebarAttribute.Sca
          * Rotate the label to some degree.
          */
         @HasDefaultValue
-        public int labelRotation = 0;
+        public float labelRotation = 0.0f;
 
         /**
          * The horizontal alignment of the scalebar inside the scalebar graphic (default: left).
@@ -367,10 +367,10 @@ public class ScalebarAttribute extends ReflectiveAttribute<ScalebarAttribute.Sca
         }
 
         /**
-         * @return Return the label rotation in degree
+         * @return Return the label rotation
          */
-        public final int getLabelRotation() {
-            return this.labelRotation;
+        public final float getLabelRotation() {
+            return (float) Math.toRadians(this.labelRotation);
         }
 
         /**

core/src/main/java/org/mapfish/print/attribute/map/GenericMapAttribute.java
@@ -472,8 +472,16 @@ public abstract class GenericMapAttribute
             return this.height;
         }
 
+        /**
+         * Gets the rotation.
+         *
+         * @return the rotation
+         */
         public Double getRotation() {
-            return this.rotation;
+            if (this.rotation == null) {
+                return null;
+            }
+            return Math.toRadians(this.rotation);
         }
 
         public String getProjection() {

core/src/main/java/org/mapfish/print/attribute/map/MapfishMapContext.java
@@ -31,17 +31,33 @@ public final class MapfishMapContext {
      * Constructor.
      * @param bounds the map bounds
      * @param mapSize the map size
-     * @param rotationInDegree the rotation in degree
      * @param dpi the dpi of the printed map
      * @param requestorDpi the dpi of the client map
      * @param forceLongitudeFirst If true then force longitude coordinates as the first coordinate.
      * @param dpiSensitiveStyle Scale the vector styles?
      */
     public MapfishMapContext(
-            final MapBounds bounds, final Dimension mapSize, final double rotationInDegree,
+            final MapBounds bounds, final Dimension mapSize,
             final double dpi, final double requestorDpi, final Boolean forceLongitudeFirst,
             final boolean dpiSensitiveStyle) {
-        this(null, bounds, mapSize, rotationInDegree, dpi, requestorDpi, forceLongitudeFirst,
+        this(null, bounds, mapSize, dpi, requestorDpi, forceLongitudeFirst,
+                dpiSensitiveStyle);
+    }
+    /**
+     * Constructor.
+     * @param bounds the map bounds
+     * @param mapSize the map size
+     * @param rotation the rotation
+     * @param dpi the dpi of the printed map
+     * @param requestorDpi the dpi of the client map
+     * @param forceLongitudeFirst If true then force longitude coordinates as the first coordinate.
+     * @param dpiSensitiveStyle Scale the vector styles?
+     */
+    public MapfishMapContext(
+            final MapBounds bounds, final Dimension mapSize, final double rotation,
+            final double dpi, final double requestorDpi, final Boolean forceLongitudeFirst,
+            final boolean dpiSensitiveStyle) {
+        this(null, bounds, mapSize, rotation, dpi, requestorDpi, forceLongitudeFirst,
                 dpiSensitiveStyle);
     }
 
@@ -50,7 +66,6 @@ public final class MapfishMapContext {
      * @param parent the context that this context is derived from
      * @param bounds the map bounds
      * @param mapSize the map size
-     * @param rotationInDegree the rotation in degree
      * @param dpi the dpi of the printed map
      * @param requestorDpi the dpi of the client map
      * @param forceLongitudeFirst If true then force longitude coordinates as the first coordinate.
@@ -59,9 +74,9 @@ public final class MapfishMapContext {
     // CSOFF: ParameterNumber
     public MapfishMapContext(
             final MapfishMapContext parent, final MapBounds bounds, final Dimension mapSize,
-            final double rotationInDegree, final double dpi, final double requestorDpi,
+            final double dpi, final double requestorDpi,
             final Boolean forceLongitudeFirst, final boolean dpiSensitiveStyle) {
-        this(parent, bounds, mapSize, rotationInDegree, true, dpi, requestorDpi, forceLongitudeFirst,
+        this(parent, bounds, mapSize, 0, dpi, requestorDpi, forceLongitudeFirst,
                 dpiSensitiveStyle);
     }
 
@@ -71,7 +86,6 @@ public final class MapfishMapContext {
      * @param bounds the map bounds
      * @param mapSize the map size
      * @param rotation the rotation
-     * @param rotationInDegree the rotation in degree?
      * @param dpi the dpi of the printed map
      * @param requestorDpi the dpi of the client map
      * @param forceLongitudeFirst If true then force longitude coordinates as the first coordinate.
@@ -80,13 +94,13 @@ public final class MapfishMapContext {
     // CSOFF: ParameterNumber
     public MapfishMapContext(
             final MapfishMapContext parent, final MapBounds bounds, final Dimension mapSize,
-            final double rotation, final boolean rotationInDegree, final double dpi,
+            final double rotation, final double dpi,
             final double requestorDpi, final Boolean forceLongitudeFirst, final boolean dpiSensitiveStyle) {
         // CSON: ParameterNumber
         this.parent = parent;
         this.bounds = bounds;
         this.mapSize = mapSize;
-        this.rotation = rotationInDegree ? Math.toRadians(rotation) : rotation;
+        this.rotation = rotation;
         this.dpi = dpi;
         this.requestorDpi = requestorDpi;
         this.forceLongitudeFirst = forceLongitudeFirst == null ? false : forceLongitudeFirst;
@@ -98,6 +112,13 @@ public final class MapfishMapContext {
      */
     public double getRotation() {
         return this.rotation;
+    }
+
+    /**
+     * @return The rotation in degree.
+     */
+    public double getRotationDegree() {
+        return Math.toDegrees(this.rotation);
     }
 
     public MapBounds getBounds() {

core/src/main/java/org/mapfish/print/http/MfClientHttpRequestFactoryImpl.java
@@ -68,8 +68,8 @@ public class MfClientHttpRequestFactoryImpl extends HttpComponentsClientHttpRequ
 
     // allow extension only for testing
     @Override
-    public ConfigurableRequest createRequest(@Nonnull final URI uri,
-                                             @Nonnull final HttpMethod httpMethod) throws IOException {
+    public ConfigurableRequest createRequest(
+            @Nonnull final URI uri, @Nonnull final HttpMethod httpMethod) throws IOException {
         HttpRequestBase httpRequest = (HttpRequestBase) createHttpUriRequest(httpMethod, uri);
         return new Request(getHttpClient(), httpRequest, createHttpContext(httpMethod, uri));
     }

core/src/main/java/org/mapfish/print/map/geotools/AbstractFeatureSourceLayer.java
@@ -57,8 +57,9 @@ public abstract class AbstractFeatureSourceLayer extends AbstractGeotoolsLayer {
      * @param httpRequestFactory The factory for making http requests.
      * @param mapContext The map context.
      */
-    public final FeatureSource<?, ?> getFeatureSource(final MfClientHttpRequestFactory httpRequestFactory,
-            final MapfishMapContext mapContext) {
+    public final FeatureSource<?, ?> getFeatureSource(
+            @Nonnull final MfClientHttpRequestFactory httpRequestFactory,
+            @Nonnull final MapfishMapContext mapContext) {
         if (this.featureSource == null) {
             this.featureSource = this.featureSourceSupplier.load(httpRequestFactory, mapContext);
         }
@@ -66,8 +67,9 @@ public abstract class AbstractFeatureSourceLayer extends AbstractGeotoolsLayer {
     }
 
     @Override
-    public final List<? extends Layer> getLayers(final MfClientHttpRequestFactory httpRequestFactory,
-                                                 final MapfishMapContext mapContext) throws Exception {
+    public final List<? extends Layer> getLayers(
+            @Nonnull final MfClientHttpRequestFactory httpRequestFactory,
+            @Nonnull final MapfishMapContext mapContext) throws Exception {
         FeatureSource<?, ?> source = getFeatureSource(httpRequestFactory, mapContext);
         Style style = this.styleSupplier.load(httpRequestFactory, source);
 

core/src/main/java/org/mapfish/print/map/geotools/AbstractGeotoolsLayer.java
@@ -177,7 +177,6 @@ public abstract class AbstractGeotoolsLayer implements MapLayer {
                     transformer.getRotatedBoundsAdjustedForPreciseRotatedMapSize(),
                     transformer.getRotatedMapSize(),
                     0,
-                    false,
                     transformer.getDPI(),
                     transformer.getRequestorDPI(),
                     transformer.isForceLongitudeFirst(),

core/src/main/java/org/mapfish/print/map/geotools/grid/GridLayer.java
@@ -25,6 +25,8 @@ import java.awt.geom.Rectangle2D;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 
+import javax.annotation.Nonnull;
+
 /**
  * A layer which is a spatial grid of lines on the map.
  */
@@ -102,8 +104,7 @@ public final class GridLayer implements MapLayer {
 
             applyOffset(transform, label.side);
 
-            final double rotationDegrees = Math.toDegrees(transformer.getRotation());
-            RotationQuadrant.getQuadrant(rotationDegrees, this.params.rotateLabels)
+            RotationQuadrant.getQuadrant(transformer.getRotation(), this.params.rotateLabels)
                     .updateTransform(transform, this.params.indent, label.side,
                             halfCharHeight, textBounds);
             graphics2D.setTransform(transform);
@@ -142,8 +143,9 @@ public final class GridLayer implements MapLayer {
     }
 
     @VisibleForTesting
-    List<? extends Layer> getLayers(final MfClientHttpRequestFactory httpRequestFactory,
-                                    final MapfishMapContext mapContext) throws Exception {
+    List<? extends Layer> getLayers(
+            @Nonnull final MfClientHttpRequestFactory httpRequestFactory,
+            @Nonnull final MapfishMapContext mapContext) throws Exception {
         return this.grid.getLayers(httpRequestFactory, mapContext);
     }
 

commit efbaf1253e2a2e21384bf04fc61ab245f986bd57

Fix scalebar label rotation bug
Regression introduced in ad0a40e8cea.
Fixed the expected images.

core/src/main/java/org/mapfish/print/processor/map/scalebar/ScalebarDrawer.java
@@ -185,7 +185,7 @@ public abstract class ScalebarDrawer {
                 newMargin = label.getGraphicOffset() - halfRotatedHeight;
             }
             if (labelRotation != 0.0) {
-                centerTransform.concatenate(AffineTransform.getRotateInstance(Math.toRadians(labelRotation)));
+                centerTransform.concatenate(AffineTransform.getRotateInstance(labelRotation));
             }
 
             if (!shouldSkipLabel) {

core/src/main/java/org/mapfish/print/processor/map/scalebar/ScalebarGraphic.java
@@ -128,7 +128,7 @@ public class ScalebarGraphic {
     /**
      * Try recursively to find the correct layout.
      */
-    private void tryLayout(final Graphics2D graphics2D, final DistanceUnit scaleUnit, final double scaleDenominator,
+    private static void tryLayout(final Graphics2D graphics2D, final DistanceUnit scaleUnit, final double scaleDenominator,
             final double intervalLengthInWorldUnits, final ScaleBarRenderSettings settings, final int tryNumber) {
         if (tryNumber > MAX_NUMBER_LAYOUTING_TRIES) {
             // if no good layout can be found, stop. an empty scalebar graphic will be shown.
@@ -163,10 +163,10 @@ public class ScalebarGraphic {
                 TextLayout labelLayout = new TextLayout(labelText, font, frc);
                 labels.add(new Label(intervalLengthInPixels * i, labelLayout, graphics2D));
             }
-            leftLabelMargin = labels.get(0).getRotatedWidth(scalebarParams.labelRotation) / 2.0f;
-            rightLabelMargin = labels.get(labels.size() - 1).getRotatedWidth(scalebarParams.labelRotation) / 2.0f;
-            topLabelMargin = labels.get(0).getRotatedHeight(scalebarParams.labelRotation) / 2.0f;
-            bottomLabelMargin = labels.get(labels.size() - 1).getRotatedHeight(scalebarParams.labelRotation) / 2.0f;
+            leftLabelMargin = labels.get(0).getRotatedWidth(scalebarParams.getLabelRotation()) / 2.0f;
+            rightLabelMargin = labels.get(labels.size() - 1).getRotatedWidth(scalebarParams.getLabelRotation()) / 2.0f;
+            topLabelMargin = labels.get(0).getRotatedHeight(scalebarParams.getLabelRotation()) / 2.0f;
+            bottomLabelMargin = labels.get(labels.size() - 1).getRotatedHeight(scalebarParams.getLabelRotation()) / 2.0f;
         } else {
             //if there is only one interval, place the label centered between the two tick marks
             String labelText = createLabelText(scaleUnit, intervalLengthInWorldUnits, intervalUnit) + intervalUnit;
@@ -200,7 +200,7 @@ public class ScalebarGraphic {
         }
     }
 
-    private boolean fitsAvailableSpace(final ScalebarAttributeValues scalebarParams,
+    private static boolean fitsAvailableSpace(final ScalebarAttributeValues scalebarParams,
             final float intervalWidthInPixels, final float leftLabelMargin,
             final float rightLabelMargin, final float topLabelMargin,
             final float bottomLabelMargin, final ScaleBarRenderSettings settings) {
@@ -218,7 +218,7 @@ public class ScalebarGraphic {
      * <p></p>
      * Creates the drawer which draws the scalebar.
      */
-    private void doLayout(final Graphics2D graphics2d, final ScalebarAttributeValues scalebarParams,
+    private static void doLayout(final Graphics2D graphics2d, final ScalebarAttributeValues scalebarParams,
             final ScaleBarRenderSettings settings) {
         final Dimension maxLabelSize = getMaxLabelSize(settings);
 
@@ -257,11 +257,11 @@ public class ScalebarGraphic {
                 + settings.getLeftLabelMargin() + settings.getRightLabelMargin();
             height = 2 * settings.getPadding()
                 + settings.getBarSize() + settings.getLabelDistance()
-                + Label.getRotatedHeight(maxLabelSize, scalebarParams.labelRotation);
+                + Label.getRotatedHeight(maxLabelSize, scalebarParams.getLabelRotation());
         } else {
             width = 2 * settings.getPadding()
                 + settings.getLabelDistance() + settings.getBarSize()
-                + Label.getRotatedWidth(maxLabelSize, scalebarParams.labelRotation);
+                + Label.getRotatedWidth(maxLabelSize, scalebarParams.getLabelRotation());
             height = 2 * settings.getPadding()
                 + settings.getTopLabelMargin()
                 + settings.getIntervalLengthInPixels() * scalebarParams.intervals
@@ -306,7 +306,7 @@ public class ScalebarGraphic {
         }
     }
 
-    private DistanceUnit bestUnit(final DistanceUnit scaleUnit, final double intervalDistance, final boolean lockUnits) {
+    private static DistanceUnit bestUnit(final DistanceUnit scaleUnit, final double intervalDistance, final boolean lockUnits) {
         if (lockUnits) {
             return scaleUnit;
         } else {
@@ -322,7 +322,7 @@ public class ScalebarGraphic {
      * @param lockUnits if set, the values are not scaled to a "nicer" unit.
      */
     @VisibleForTesting
-    protected final double getNearestNiceValue(final double value, final DistanceUnit scaleUnit, final boolean lockUnits) {
+    protected static double getNearestNiceValue(final double value, final DistanceUnit scaleUnit, final boolean lockUnits) {
         DistanceUnit bestUnit = bestUnit(scaleUnit, value, lockUnits);
         double factor = scaleUnit.convertTo(1.0, bestUnit);
 
@@ -352,7 +352,7 @@ public class ScalebarGraphic {
     /**
      * @return The "nicest" number of sub intervals in function of the interval distance.
      */
-    private int getNbSubIntervals(final DistanceUnit scaleUnit, final double intervalDistance, final DistanceUnit intervalUnit) {
+    private static int getNbSubIntervals(final DistanceUnit scaleUnit, final double intervalDistance, final DistanceUnit intervalUnit) {
         double value = scaleUnit.convertTo(intervalDistance, intervalUnit);
         int digits = (int) (Math.log(value) / Math.log(10));
         double pow10 = Math.pow(10, digits);
@@ -373,7 +373,7 @@ public class ScalebarGraphic {
         }
     }
 
-    private int getFontSize(final ScaleBarRenderSettings settings) {
+    private static int getFontSize(final ScaleBarRenderSettings settings) {
         return (int) Math.ceil(settings.getParams().fontSize * settings.getDpiRatio());
     }
 

core/src/test/java/org/mapfish/print/processor/map/scalebar/ScalebarDrawerTest.java
@@ -92,7 +92,7 @@ public class ScalebarDrawerTest {
         drawer.draw();
 
         new ImageSimilarity(getFile("expected-scalebar-line-labels-partially-rotated.png"))
-                .assertSimilarity(bufferedImage, 200);
+                .assertSimilarity(bufferedImage, 270);
     }
 
     @Test
@@ -130,7 +130,7 @@ public class ScalebarDrawerTest {
         drawer.draw();
 
         new ImageSimilarity(getFile("expected-scalebar-line-labels-above-partially-rotated.png")).
-                assertSimilarity(bufferedImage, 85);
+                assertSimilarity(bufferedImage, 210);
     }
 
     @Test
@@ -149,7 +149,7 @@ public class ScalebarDrawerTest {
         drawer.draw();
 
         new ImageSimilarity(getFile("expected-scalebar-line-labels-left-rotated.png"))
-                .assertSimilarity(bufferedImage, 160);
+                .assertSimilarity(bufferedImage, 210);
     }
 
     @Test
@@ -168,7 +168,7 @@ public class ScalebarDrawerTest {
         drawer.draw();
 
         new ImageSimilarity(getFile("expected-scalebar-line-labels-left-partially-rotated.png"))
-                .assertSimilarity(bufferedImage, 190);
+                .assertSimilarity(bufferedImage, 210);
     }
 
     @Test
@@ -187,7 +187,7 @@ public class ScalebarDrawerTest {
         drawer.draw();
 
         new ImageSimilarity(getFile("expected-scalebar-line-labels-right-rotated.png"))
-                .assertSimilarity(bufferedImage, 100);
+                .assertSimilarity(bufferedImage, 130);
     }
 
     @Test
@@ -206,7 +206,7 @@ public class ScalebarDrawerTest {
         drawer.draw();
 
         new ImageSimilarity(getFile("expected-scalebar-line-labels-right-partially-rotated.png"))
-                .assertSimilarity(bufferedImage, 85);
+                .assertSimilarity(bufferedImage, 100);
     }
 
     @Test