ad82e073366ccb4b5eda6a7eedc68f04067ff720
Andreas Watermeyer <Andreas.Watermeyer@its-telco.de>
2018-01-17 10:52:47 +0100

    [#645] New matcher based on hostname, improved docs

--- core/src/main/java/org/mapfish/print/processor/http/matcher/HostnameMatcher.java
+++ core/src/main/java/org/mapfish/print/processor/http/matcher/HostnameMatcher.java
@@ -1,34 +1,33 @@
 package org.mapfish.print.processor.http.matcher;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
 import org.mapfish.print.config.Configuration;
 import org.mapfish.print.config.ConfigurationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.List;
 
 /**
- * Allows to check that a given URL matches a DNS address (textual format). The actual comparison is
- * performed on numerical IP addresses. The configured DNS host address is transformed into numerical IP 
- * addresses during application startup. The urls to be compared are transformed during the print processing.
+ * Allows to check that a given URL matches a hostname literally (textual match).
+ * 
  * <p>Example 1: Accept any www.camptocamp.com url</p>
  * <pre><code>
- *     - !dnsMatch
+ *     - !hostnameMatch
  *       host : www.camptocamp.com
  * </code></pre>
  * <p>Example 2: Accept any www.camptocamp.com url (port == -1 accepts any port)</p>
  * <pre><code>
- *     - !dnsMatch
+ *     - !hostnameMatch
  *       host : www.camptocamp.com
  *       port : -1
  * </code></pre>
  * <p>Example 3: Accept any www.camptocamp.com url on port 80 only</p>
  * <pre><code>
- *     - !dnsMatch
+ *     - !hostnameMatch
  *       host : www.camptocamp.com
  *       port : 80
  * </code></pre>
@@ -39,54 +38,59 @@ import java.util.List;
  *     </p>
  *
  * <pre><code>
- *     - !dnsMatch
+ *     - !hostnameMatch
  *       host : www.camptocamp.com
  *       pathRegex : /print/.+
  * </code></pre>
- * [[examples=http_processors]]
  */
-public class DnsHostMatcher extends HostMatcher {
-    private List<AddressHostMatcher> matchersForHost = Lists.newArrayList();
+public final class HostnameMatcher extends HostMatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HostnameMatcher.class);
     private String host;
 
     /**
-     * Check the given URI to see if it matches.
-     *
-     * @param matchInfo the matchInfo to validate.
-     *
-     * @return True if it matches.
+     * Creates a new instance.
+     */
+    public HostnameMatcher() {
+        super();
+    }
+
+    /* (non-Javadoc)
+     * @see org.mapfish.print.config.ConfigurationObject#validate(java.util.List, org.mapfish.print.config.Configuration)
      */
     @Override
-    public final Optional<Boolean> tryOverrideValidation(final MatchInfo matchInfo) throws SocketException,
-            UnknownHostException, MalformedURLException {
-        for (AddressHostMatcher addressHostMatcher : this.matchersForHost) {
-            if (addressHostMatcher.matches(matchInfo)) {
-                return Optional.absent();
-            }
-        }
-
-        return Optional.of(false);
-    }
-    @Override
-    public final void validate(final List<Throwable> validationErrors, final Configuration configuration) {
+    public void validate(final List<Throwable> validationErrors, final Configuration configuration) {
         if (this.host == null) {
             validationErrors.add(new ConfigurationException("No host defined: " + getClass().getName()));
         }
     }
 
-    /**
-     * Set the host.
-     * @param host the host
+    /* (non-Javadoc)
+     * @see org.mapfish.print.processor.http.matcher.HostMatcher#tryOverrideValidation(org.mapfish.print.processor.http.matcher.MatchInfo)
      */
-    public final void setHost(final String host) throws UnknownHostException {
-        this.host = host;
-        final InetAddress[] inetAddresses = InetAddress.getAllByName(host);
-
-        for (InetAddress address : inetAddresses) {
-            final AddressHostMatcher matcher = new AddressHostMatcher();
-            matcher.setIp(address.getHostAddress());
-            this.matchersForHost.add(matcher);
+    @Override
+    protected Optional<Boolean> tryOverrideValidation(final MatchInfo matchInfo)
+            throws UnknownHostException, SocketException, MalformedURLException {
+        String host = matchInfo.getHost();
+        if (host == MatchInfo.ANY_HOST) {
+            return Optional.absent();
         }
+        if (isHostnameMatch(host)) {
+            return Optional.absent();
+        }
+        return Optional.of(false);
+    }
+
+    /**
+     * @param host the host
+     * @return true, if the hostname matches.
+     */
+    protected boolean isHostnameMatch(final String host) {
+        boolean match = this.host.equalsIgnoreCase(host);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Configured hostname '" + this.host + "' matches requested '" + host + "': " + match);
+        }
+        return match;
     }
 
     // CHECKSTYLE:OFF
@@ -94,7 +98,7 @@ public class DnsHostMatcher extends HostMatcher {
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
-        sb.append("DnsHostMatcher");
+        sb.append("HostnameMatcher");
         sb.append("{host='").append(host).append('\'');
         if (port >= 0) {
             sb.append(", port=").append(port);
@@ -105,6 +109,14 @@ public class DnsHostMatcher extends HostMatcher {
         sb.append(", reject=").append(isReject());
         sb.append('}');
         return sb.toString();
+    }
+    
+    /**
+     * Set the host.
+     * @param host the host
+     */
+    public void setHost(final String host) throws UnknownHostException {
+        this.host = host;
     }
 
     @Override
@@ -129,7 +141,7 @@ public class DnsHostMatcher extends HostMatcher {
         if (getClass() != obj.getClass()) {
             return false;
         }
-        DnsHostMatcher other = (DnsHostMatcher) obj;
+        HostnameMatcher other = (HostnameMatcher) obj;
         if (host == null) {
             if (other.host != null) {
                 return false;
