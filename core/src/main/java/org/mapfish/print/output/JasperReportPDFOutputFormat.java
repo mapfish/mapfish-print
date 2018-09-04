0089baef54de93298e6bee511c2f1401815f1f24
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-09-04 08:17:25 +0200

    Add per printed page stats

--- core/src/main/java/org/mapfish/print/output/JasperReportPDFOutputFormat.java
+++ core/src/main/java/org/mapfish/print/output/JasperReportPDFOutputFormat.java
@@ -1,12 +1,15 @@
 package org.mapfish.print.output;
 
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.PrintPageFormat;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import net.sf.jasperreports.export.type.PdfVersionEnum;
 import org.mapfish.print.config.PDFConfig;
+import org.mapfish.print.processor.ExecutionStats;
 
 import java.io.OutputStream;
 
@@ -49,5 +52,12 @@ public final class JasperReportPDFOutputFormat extends AbstractJasperReportOutpu
         exporter.setConfiguration(configuration);
 
         exporter.exportReport();
+
+        final JasperPrint jasperPrint = exporter.getCurrentJasperPrint();
+        final ExecutionStats stats = print.executionContext.getStats();
+        for (int i = 0; i < jasperPrint.getPages().size(); ++i) {
+            final PrintPageFormat pageFormat = jasperPrint.getPageFormat(i);
+            stats.addPageStats(pageFormat);
+        }
     }
 }
