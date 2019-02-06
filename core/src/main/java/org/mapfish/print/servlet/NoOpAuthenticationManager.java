ea4de5b9a972e7613bb54e4f0f90c5ddc612890d
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2019-02-06 15:45:07 +0100

    Disable authentication by default
    Customers had problems with c2cgeoportal print proxy forwarding basic
    auth to the print that didn't have any special auth setup.
    
    Instead of failing, the print acts normal (not authenticated) when some random
    user/password are used.
    
    https://jira.camptocamp.com/browse/GEO-1816

---
+++ core/src/main/java/org/mapfish/print/servlet/NoOpAuthenticationManager.java
@@ -0,0 +1,59 @@
+package org.mapfish.print.servlet;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * A no-op AuthenticationManager.
+ */
+public class NoOpAuthenticationManager implements AuthenticationManager {
+    @Override
+    public Authentication authenticate(
+            final Authentication authentication) throws AuthenticationException {
+        return new Authentication() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return Collections.EMPTY_LIST;
+            }
+
+            @Override
+            public Object getCredentials() {
+                return "";
+            }
+
+            @Override
+            public Object getDetails() {
+                return null;
+            }
+
+            @Override
+            public Object getPrincipal() {
+                return "";
+            }
+
+            @Override
+            public boolean isAuthenticated() {
+                return false;
+            }
+
+            @Override
+            public void setAuthenticated(final boolean isAuthenticated) throws IllegalArgumentException {
+
+            }
+
+            @Override
+            public String getName() {
+                return "anonymous";
+            }
+        };
+    }
+
+    public boolean isEraseCredentialsAfterAuthentication() {
+        return true;
+    }
+}
