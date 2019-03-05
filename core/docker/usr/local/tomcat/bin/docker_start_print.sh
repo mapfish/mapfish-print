7cfc0d684d432f8f4c8cdea514eff6f4cf7442ca
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2019-03-05 15:30:23 +0100

    Add EXTRA_JARS variable to the config of the Docker image

--- core/docker/usr/local/tomcat/bin/docker_start_print.sh
+++ core/docker/usr/local/tomcat/bin/docker_start_print.sh
@@ -7,6 +7,12 @@ then
        WEB-INF/classes/mapfish-spring-application-context-override.xml
 fi
 
+if [[ -n "${EXTRA_JARS}" ]]
+then
+    echo "Adding jar files:"
+    cp -v "${EXTRA_JARS}"/*.jar WEB-INF/lib/
+fi
+
 mkdir -p print-apps
 rm -f /usr/local/tomcat/temp/mapfish-print/ROOT/stop /usr/local/tomcat/temp/mapfish-print/ROOT/stopped
 exec catalina.sh run
