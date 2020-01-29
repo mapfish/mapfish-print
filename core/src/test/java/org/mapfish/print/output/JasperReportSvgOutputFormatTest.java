476634732b2cb4cec12c02473b673876b92e45b8
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-01-29 16:09:51 +0100

    Fix the tests in CI

--- core/src/test/java/org/mapfish/print/output/JasperReportSvgOutputFormatTest.java
+++ core/src/test/java/org/mapfish/print/output/JasperReportSvgOutputFormatTest.java
@@ -6,7 +6,11 @@ import org.mapfish.print.config.ConfigurationFactory;
 import org.mapfish.print.wrapper.json.PJsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -28,6 +32,15 @@ public class JasperReportSvgOutputFormatTest extends AbstractJasperReportOutputF
         format.print("test", requestData, config,
                 getFile(JasperReportSvgOutputFormatTest.class, BASE_DIR), getTaskDirectory(),
                 outputStream);
+
+        File actual = new File(
+            getFile(BASE_DIR + "expectedReport.svg").toPath().toString().replaceFirst("test", "actual")
+                .replace("expectedReport.svg", "actualReport.svg")
+        );
+        actual.getParentFile().mkdirs();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(actual));
+        writer.write(outputStream.toString());
+        writer.close();
 
         String expected = getFileContent(BASE_DIR + "expectedReport.svg");
         assertEquals(expected, outputStream.toString());
