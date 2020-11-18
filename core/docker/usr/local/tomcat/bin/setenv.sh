7173250ed4761365fc3aece9487c24a47fcf79a3
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-11-18 13:40:40 +0100

    Add some logs

---
+++ core/docker/usr/local/tomcat/bin/setenv.sh
@@ -0,0 +1,10 @@
+CLASSPATH=${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar
+
+for jar in ${CATALINA_HOME}/lib/*
+do
+    CLASSPATH=${CLASSPATH}:$jar
+done
+
+UMASK="0022"
+
+CATALINA_OPTS="${CATALINA_OPTS} -Dlogback.configurationFile=${CATALINA_HOME}/conf/logback.xml"
