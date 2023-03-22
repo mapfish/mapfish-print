1e70d1e13f8165880ffafdb49b1e406060719591
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-03-22 11:36:24 +0100

    Fix shell checks

--- core/docker/usr/local/tomcat/bin/setenv.sh
+++ core/docker/usr/local/tomcat/bin/setenv.sh
@@ -1,9 +1,11 @@
-CLASSPATH=${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar
+#!/bin/bash
 
-for jar in ${CATALINA_HOME}/lib/*; do
-    CLASSPATH=${CLASSPATH}:$jar
+CLASSPATH="${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar"
+
+for jar in "${CATALINA_HOME}/lib/"*; do
+    "CLASSPATH=${CLASSPATH}:$jar"
 done
 
-UMASK="0022"
+export UMASK="0022"
 
 CATALINA_OPTS="${CATALINA_OPTS} -Dlogback.configurationFile=${CATALINA_HOME}/conf/logback.xml"
