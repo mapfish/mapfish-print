8233d8b5963a893ff6d20c3e3fe5a4eb6fbd469b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-09-23 10:29:03 +0200

    Acctivate access log

--- core/docker/usr/local/tomcat/bin/setenv.sh
+++ core/docker/usr/local/tomcat/bin/setenv.sh
@@ -3,7 +3,3 @@ CLASSPATH=${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.ja
 for jar in ${CATALINA_HOME}/lib/*; do
     CLASSPATH=${CLASSPATH}:$jar
 done
-
-UMASK="0022"
-
-CATALINA_OPTS="${CATALINA_OPTS} -Dlogback.configurationFile=${CATALINA_HOME}/conf/logback.xml"
