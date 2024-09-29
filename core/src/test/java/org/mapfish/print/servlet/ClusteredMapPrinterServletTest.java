2f6051892614916f2fdb29f15ade95201444ae0a
christophe mangeat <christophe.mangeat@camptocamp.com>
2024-09-29 23:00:33 +0200

    Test context H2 datasource

--- core/src/test/java/org/mapfish/print/servlet/ClusteredMapPrinterServletTest.java
+++ core/src/test/java/org/mapfish/print/servlet/ClusteredMapPrinterServletTest.java
@@ -1,12 +1,10 @@
 package org.mapfish.print.servlet;
 
-import org.junit.Ignore;
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration(locations = {ClusteredMapPrinterServletTest.CLUSTERED_CONTEXT})
-@Ignore // db must be set up to run this test
 public class ClusteredMapPrinterServletTest extends MapPrinterServletTest {
 
   public static final String CLUSTERED_CONTEXT =
-      "classpath:org/mapfish/print/servlet/mapfish-spring-application-context-clustered.xml";
+      "classpath:org/mapfish/print/servlet/mapfish-spring-application-context-clustered-test.xml";
 }
