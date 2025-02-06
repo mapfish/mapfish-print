b04379617d9970d168033b40f55d15b6bb3311a7
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2025-02-06 16:24:45 +0100

    Removed debug output, added exception throwing

--- docs/src/main/groovy/Javadoc7Parser.groovy
+++ docs/src/main/groovy/Javadoc7Parser.groovy
@@ -58,9 +58,7 @@ class Javadoc7Parser {
                     throw new IllegalArgumentException(errorHandler(obj, objectName))
                 }
             } else {
-                System.out.printf("HERE-MISS: %s\n", errorHandler(obj, objectName))
-                return ""
-                //throw new IllegalArgumentException(errorHandler(obj, objectName))
+                throw new IllegalArgumentException(errorHandler(obj, objectName))
             }
         }
     }
