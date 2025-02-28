df849334c18522c7c0c70247502cb11d3143b61d
sebr72 <sebastien_riollet@hotmail.com>
2025-02-28 13:48:21 +0100

    Prepare hibernate migration

--- core/src/main/java/org/mapfish/print/servlet/job/impl/PrintJobResultImpl.java
+++ core/src/main/java/org/mapfish/print/servlet/job/impl/PrintJobResultImpl.java
@@ -1,14 +1,14 @@
 package org.mapfish.print.servlet.job.impl;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.net.URI;
 import java.net.URISyntaxException;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.Type;
--- core/src/main/java/org/mapfish/print/servlet/job/impl/PrintJobStatusImpl.java
+++ core/src/main/java/org/mapfish/print/servlet/job/impl/PrintJobStatusImpl.java
@@ -1,16 +1,16 @@
 package org.mapfish.print.servlet.job.impl;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.Date;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import org.hibernate.annotations.Target;
 import org.hibernate.annotations.Type;
 import org.mapfish.print.config.access.AccessAssertion;
