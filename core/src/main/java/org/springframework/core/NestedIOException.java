bb383091160e593f5eacacec6d465c136cb50db0
Andrey Rusakov (andrey.rusakov@camptocamp.com) <lanseg@proton.me>
2025-05-06 18:27:00 +0200

    Add exception class wanted by the code to remove it later

---
+++ core/src/main/java/org/springframework/core/NestedIOException.java
@@ -0,0 +1,27 @@
+package org.springframework.core;
+
+import java.io.IOException;
+
+public class NestedIOException extends IOException {
+
+    static {
+        NestedExceptionUtils.class.getName();
+    }
+
+    public NestedIOException(final String msg) {
+        super(msg);
+    }
+
+    public NestedIOException(final String msg, final Throwable cause) {
+        super(msg, cause);
+    }
+
+    /**
+     * @return something
+     */
+    @Override
+    public String getMessage() {
+        return NestedExceptionUtils.buildMessage(super.getMessage(), getCause());
+    }
+
+}
