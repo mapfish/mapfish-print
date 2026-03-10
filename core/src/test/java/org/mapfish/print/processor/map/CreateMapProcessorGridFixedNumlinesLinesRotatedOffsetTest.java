ba7afabd5d80b0d7cf4a2067164241e0bc450f7e
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-03-10 13:15:56 +0100

    Add regression test for Grid Layer with rotation and offsets
    Add CreateMapProcessorGridFixedNumlinesLinesRotatedOffsetTest to verify that label offsets are applied correctly (screen-relative) when the map is rotated.

---
+++ core/src/test/java/org/mapfish/print/processor/map/CreateMapProcessorGridFixedNumlinesLinesRotatedOffsetTest.java
@@ -0,0 +1,88 @@
+package org.mapfish.print.processor.map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.jupiter.api.Test;
+import org.mapfish.print.AbstractMapfishSpringTest;
+import org.mapfish.print.TestHttpClientFactory;
+import org.mapfish.print.config.Configuration;
+import org.mapfish.print.config.ConfigurationFactory;
+import org.mapfish.print.config.Template;
+import org.mapfish.print.output.Values;
+import org.mapfish.print.test.util.ImageSimilarity;
+import org.mapfish.print.wrapper.json.PJsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Basic test of the Map processor.
+ *
+ * <p>Created by Jesse on 3/26/14.
+ */
+public class CreateMapProcessorGridFixedNumlinesLinesRotatedOffsetTest
+    extends AbstractMapfishSpringTest {
+  public static final String BASE_DIR = "grid_numlines_lines_fixedscale_rotated_offsets/";
+
+  @Autowired private ConfigurationFactory configurationFactory;
+  @Autowired private TestHttpClientFactory requestFactory;
+  @Autowired private ForkJoinPool forkJoinPool;
+
+  private static PJsonObject loadJsonRequestData() throws IOException {
+    return parseJSONObjectFromFile(
+        CreateMapProcessorGridFixedNumlinesLinesRotatedOffsetTest.class,
+        BASE_DIR + "requestData.json");
+  }
+
+  @Test
+  @DirtiesContext
+  public void testExecute() throws Exception {
+    final String host = "center_osm_fixedscale";
+    requestFactory.registerHandler(
+        input ->
+            (("" + input.getHost()).contains(host + ".osm"))
+                || input.getAuthority().contains(host + ".osm"),
+        createFileHandler(uri -> "/map-data/osm" + uri.getPath()));
+
+    final Configuration config = configurationFactory.getConfig(getFile(BASE_DIR + "config.yaml"));
+    final Template template = config.getTemplate("main");
+    PJsonObject requestData = loadJsonRequestData();
+    PJsonObject map = requestData.getJSONObject("attributes").getJSONObject("map");
+    // Only check a couple of rotations to verify offsets work with rotation
+    int[] rotationsToTest = {0, 90, 45};
+
+    for (int rotation : rotationsToTest) {
+      map.getInternalObj().put("rotation", rotation);
+      Values values =
+          new Values(
+              new HashMap<>(),
+              requestData,
+              template,
+              getTaskDirectory(),
+              this.requestFactory,
+              new File("."),
+              HTTP_REQUEST_MAX_NUMBER_FETCH_RETRY,
+              HTTP_REQUEST_FETCH_RETRY_INTERVAL_MILLIS,
+              new AtomicBoolean(false));
+
+      final ForkJoinTask<Values> taskFuture =
+          this.forkJoinPool.submit(template.getProcessorGraph().createTask(values));
+      taskFuture.get();
+
+      @SuppressWarnings("unchecked")
+      List<URI> layerGraphics = (List<URI>) values.getObject("layerGraphics", List.class);
+      assertEquals(2, layerGraphics.size());
+
+      String imageName = getExpectedImageName("_" + rotation, BASE_DIR);
+      new ImageSimilarity(new File(getFile(BASE_DIR), imageName))
+          .assertSimilarity(layerGraphics, 780, 330, 0);
+    }
+  }
+}
