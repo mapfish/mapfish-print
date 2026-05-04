07d321f9656654ba0e6f483ffbe1e5aff4bcd2ed
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-05-04 12:12:44 +0000

    Fix TableProcessorTest to access Jasper print field

--- core/src/test/java/org/mapfish/print/processor/jasper/TableProcessorTest.java
+++ core/src/test/java/org/mapfish/print/processor/jasper/TableProcessorTest.java
@@ -203,15 +203,14 @@ public class TableProcessorTest extends AbstractMapfishSpringTest {
 
         Values values =
                 new Values(
-                        new HashMap<>(),
+                        "test",,
                         requestData,
                         template,
                         getTaskDirectory(),
                         this.httpRequestFactory,
                         new File("."),
                         HTTP_REQUEST_MAX_NUMBER_FETCH_RETRY,
-                        HTTP_REQUEST_FETCH_RETRY_INTERVAL_MILLIS,
-                        new AtomicBoolean(false));
+                        HTTP_REQUEST_FETCH_RETRY_INTERVAL_MILLIS);
         forkJoinPool.invoke(template.getProcessorGraph().createTask(values));
 
         final JRMapCollectionDataSource tableDataSource =
@@ -240,7 +239,7 @@ public class TableProcessorTest extends AbstractMapfishSpringTest {
         JasperPrint print =
                 format
                         .getJasperPrint(new HashMap<>(), requestData, config, file, getTaskDirectory())
-                        .print();
+                        .print;
 
         new ImageSimilarity(getFile(baseDir + "expectedImage-quoted.png"))
                 .assertSimilarity(print, 0, 0);
